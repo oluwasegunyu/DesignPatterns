6f6a3fdf46877a785e7ae4cd1334abd0b4876172
yxy <charlay_yu@163.com>
2021-07-26 15:19:10 +0800

    饿汉模式

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,16 +1,9 @@
 public class Singleton {
-    private static volatile Singleton instance;
+    private final static Singleton instance = new Singleton();
 
     private Singleton(){}
 
     public static Singleton getInstance(){
-        if(instance == null) {
-            synchronized (Singleton.class) {
-                if(instance == null) {
-                    instance = new Singleton();
-                }
-            }
-        }
         return instance;
     }
 }
