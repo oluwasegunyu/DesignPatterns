957c4a9cfeccf188688c52ea8580c40ca2065aaf
yxy <charlay_yu@163.com>
2021-07-27 11:17:29 +0800

    职责链模式

---
+++ src/ConcreteHandler1.java
@@ -0,0 +1,10 @@
+public class ConcreteHandler1 extends Handler{
+    @Override
+    public void handleRequest(int request){
+        if(request>=0 && request<10){
+            System.out.println(this.getClass().getName() + "处理请求" + request);
+        }else if(successor != null){
+            successor.handleRequest(request);
+        }
+    }
+}
---
+++ src/ConcreteHandler3.java
@@ -0,0 +1,10 @@
+public class ConcreteHandler3 extends Handler{
+    @Override
+    public void handleRequest(int request){
+        if(request>=20 && request<30){
+            System.out.println(this.getClass().getName() + "处理请求" + request);
+        }else if(successor != null){
+            successor.handleRequest(request);
+        }
+    }
+}
