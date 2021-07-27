1733cf6b92e131e535e50a3a21f35183ab3c8864
yxy <charlay_yu@163.com>
2021-07-27 10:17:48 +0800

    紧耦合实现

---
+++ src/Barbecuer.java
@@ -0,0 +1,9 @@
+public class Barbecuer {
+    public void bakeMutton(){
+        System.out.println("烤羊肉串");
+    }
+
+    public void bakeChickenWing(){
+        System.out.println("烤鸡翅");
+    }
+}
---
+++ src/BarbecuerTest.java
@@ -0,0 +1,10 @@
+public class BarbecuerTest {
+    public static void main(String... args){
+        Barbecuer boy = new Barbecuer();
+        boy.bakeMutton();
+        boy.bakeMutton();
+        boy.bakeChickenWing();
+        boy.bakeMutton();
+        boy.bakeChickenWing();
+    }
+}
