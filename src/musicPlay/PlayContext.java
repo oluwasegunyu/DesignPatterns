a0554cb8f40ca9b5a4926786c2fa8ab007cb8152
yxy <charlay_yu@163.com>
2021-07-29 09:59:34 +0800

    复杂示例解释器

---
+++ src/musicPlay/PlayContext.java
@@ -0,0 +1,13 @@
+package musicPlay;
+
+public class PlayContext {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
