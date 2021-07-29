50c0dc74cd3fe7ec292d85975b4418b2be66560a
yxy <charlay_yu@163.com>
2021-07-29 09:01:21 +0800

    简单示例解释器

---
+++ src/AbstractExpression.java
@@ -0,0 +1,3 @@
+public abstract class AbstractExpression {
+    public abstract void interpret(Context context);
+}
---
+++ src/NonterminalExpression.java
@@ -0,0 +1,6 @@
+public class NonterminalExpression extends AbstractExpression{
+    @Override
+    public void interpret(Context context){
+        System.out.println("非终端解释器");
+    }
+}
